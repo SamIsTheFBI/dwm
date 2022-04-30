992133687d2f5936f99e362dddaffe6f2087e2ad
SamIsTheFBI <mrqube001@gmail.com>
2022-04-30 21:39:36 +0530

    edited readme & made patches directory

--- config.h
+++ config.h
@@ -1,11 +1,11 @@
 /* appearance */
 static const unsigned int borderpx  = 2;        /* border pixel of windows */
-static const Gap default_gap	    = {.isgap = 1, .realgap = 10, .gappx = 0};   /* gappx sets the default gap width*/
+static const Gap default_gap	    = {.isgap = 1, .realgap = 5, .gappx = 0};   /* gappx sets the default gap width*/
 static const unsigned int snap      = 32;       /* snap pixel */
-static const unsigned int gappih    = 10;       /* horiz inner gap between windows */
-static const unsigned int gappiv    = 10;       /* vert inner gap between windows */
-static const unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov    = 10;       /* vert outer gap between windows and screen edge */
+static const unsigned int gappih    = 5;       /* horiz inner gap between windows */
+static const unsigned int gappiv    = 5;       /* vert inner gap between windows */
+static const unsigned int gappoh    = 5;       /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov    = 5;       /* vert outer gap between windows and screen edge */
 static const int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
