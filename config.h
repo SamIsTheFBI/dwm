05c0d4fc7935c9131a8a99bcf2816788465b59bb
SamIsTheFBI <mrqube001@gmail.com>
2022-05-20 23:52:38 +0530

    added underlinetags patch

--- config.h
+++ config.h
@@ -14,6 +14,10 @@ static const char *fonts[]						=  { "JetBrainsMono Nerd Font:style:medium:size=
 static const char dmenufont[]					= "JetBrainsMono Nerd Font:style:medium:size=10";
 static const unsigned int baralpha		= 0xdf;
 static const unsigned int borderalpha = OPAQUE;
+static const unsigned int ulinepad	= 5;	/* horizontal padding between the underline and tag */
+static const unsigned int ulinestroke	= 2;	/* thickness / height of the underline */
+static const unsigned int ulinevoffset	= 0;	/* how far above the bottom of the bar the line should appear */
+static const int ulineall 		= 0;	/* 1 to show underline on all tags, 0 for just the active ones */
 
 #include "colors-wal-dwm.h"
 
