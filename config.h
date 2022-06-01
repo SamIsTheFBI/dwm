edd79c31873d35bb416de96ab9ff41be86796065
SamIsTheFBI <mrqube001@gmail.com>
2022-06-01 11:26:10 +0530

    colors-wal-dwm.h --> dwm-colors.h

--- config.h
+++ config.h
@@ -19,7 +19,7 @@ static const unsigned int ulinestroke	= 2;	/* thickness / height of the underlin
 static const unsigned int ulinevoffset	= 0;	/* how far above the bottom of the bar the line should appear */
 static const int ulineall 				= 0;	/* 1 to show underline on all tags, 0 for just the active ones */
 
-#include "colors-wal-dwm.h"
+#include "dwm-colors.h"
 static const char *colors[][3]      = {
     /*               fg           bg         border                         */
     [SchemeNorm] = { norm_fg,     norm_bg,   norm_border }, // unfocused wins
