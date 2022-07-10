e58dfdda816bed170c5f9a73f4fe02a939c42bc9
SamIsTheFBI <mrqube001@gmail.com>
2022-07-10 12:26:04 +0530

    added fsignal patch

--- config.def.h
+++ config.def.h
@@ -315,3 +315,10 @@ static Button buttons[] = {
     { ClkRootWin,  0, Button3, spawn, {.v = jgmenucmd } },
 };
 
+/* signal definitions */
+/* signum must be greater than 0 */
+/* trigger signals using `xsetroot -name "fsignal:<signum>"` */
+static Signal signals[] = {
+	/* signum       function        argument  */
+	{ 1,            setlayout,      {.v = 0} },
+};
